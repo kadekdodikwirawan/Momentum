ad9919ff5ca208a228eedb0c00c63bb9cf078652
Kadek Dodik Wirawan <dodikdogen@gmail.com>
2022-07-12 11:24:39 +0800

    Update app.module.ts
    Regiter sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { environment } from '../environments/environment';
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     })],
   providers: [
     AuthService,
