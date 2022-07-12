18897757422594babc3cef03003c1641015ff36b
Kadek Dodik Wirawan <dodikdogen@gmail.com>
2022-07-12 13:36:34 +0800

    Update main.ts
    Add register service worker manually

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,10 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('ngsw-worker.js')
+      .then(() => console.log('service worker installed'))
+      .catch(err => console.error('Error', err));
+  }
+}).catch(err => console.log(err));
